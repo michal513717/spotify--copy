e729b49fb288ad03205f3a6a369883b0dea3de42
Michal Kus <mkus@student.agh.edu.pl>
2022-08-28 15:06:29 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import styles from 'styles/app.module.scss'
+import React from 'react'
+
+const App: React.FC = () => {
+
+  return (
+    <div className={styles.app}>
+    </div>
+  )
+}
+
+export default App
